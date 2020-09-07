fffdea3d953ba2a407ed2df40d9067366d071f4c
OneOfOne <oneofone@gmail.com>
2020-09-06 20:11:30 -0500

    test data

--- csvticks/ticks_test.go
+++ csvticks/ticks_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestTicks(t *testing.T) {
-	const fname = "/tmp/aapl.csv.gz"
+	const fname = "testdata/AAPL.txt.gz"
 	defer SetDefaultTimeFormat(SetDefaultTimeFormat("2006-01-02 15:04:05"))
 
 	ticks, err := Load(fname, Mapping{
@@ -16,12 +16,12 @@ func TestTicks(t *testing.T) {
 		},
 
 		FillSymbol: "AAPL",
-		TS:         FieldIndex(0),
-		Open:       FieldIndex(1),
-		High:       FieldIndex(2),
-		Low:        FieldIndex(3),
-		Close:      FieldIndex(4),
-		Volume:     FieldIndex(5),
+		TS:         CSVIndex(0),
+		Open:       CSVIndex(1),
+		High:       CSVIndex(2),
+		Low:        CSVIndex(3),
+		Close:      CSVIndex(4),
+		Volume:     CSVIndex(5),
 	})
 	if err != nil {
 		t.Fatal(err)
